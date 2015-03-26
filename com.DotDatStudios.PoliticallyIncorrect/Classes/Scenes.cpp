f56c9518aa5d8bf1b4478896aba87bc95b6a30da
rNdm <rndm@windowslive.com>
2015-03-27 12:50:39 +1300

    If you reading this go fuck yourself

---
+++ com.DotDatStudios.PoliticallyIncorrect/Classes/Scenes.cpp
@@ -0,0 +1,142 @@
+#include "Scenes.h"
+
+USING_NS_CC;
+
+/**
+* IScene
+*/
+
+void IScene::menuCloseCallback(Ref* pSender)
+{
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
+	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.", "Alert");
+	return;
+#endif
+
+	Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+	exit(0);
+#endif
+};
+
+/**
+* IScene END
+*/
+
+/**
+* SplashScene
+*/
+bool SplashScene::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    // create and initialize a label
+    
+    auto label = LabelTTF::create(APP_NAME, "Marker Felt", 50);
+    
+    // position the label on the center of the screen
+    label->setPosition(Vec2(origin.x + visibleSize.width / 2, (origin.y + visibleSize.height / 2) + (label->getContentSize().height) / 2));
+    
+    label->setOpacity(0);
+
+    // add the label as a child to this layer
+    this->addChild(label, 1);
+    	
+    // fade in / out action   
+    Vector<cocos2d::FiniteTimeAction*> fadeActions(3);
+	
+    fadeActions.insert(0, CallFunc::create(CC_CALLBACK_0(SplashScene::loadMenuScene, this)));
+    fadeActions.insert(0, FadeOut::create(0.5));
+    fadeActions.insert(0, FadeIn::create(0.5));
+        
+    label->runAction(Sequence::create(fadeActions));
+    
+    return true;
+}
+
+void SplashScene::loadMenuScene()
+{        
+    auto menuScene = MenuScene::createScene();
+    Director::getInstance()->replaceScene(menuScene);
+}
+
+/**
+* SplashScene END
+*/
+
+/**
+* MenuScene
+*/
+
+bool MenuScene::init()
+{
+	//////////////////////////////
+	// 1. super init first
+	if (!Layer::init())
+	{
+		return false;
+	}
+
+	// create and initialize a menu items
+	auto menu_item_1 = MenuItemFont::create("Continue", CC_CALLBACK_1(MenuScene::Continue, this));
+	auto menu_item_2 = MenuItemFont::create("Play", CC_CALLBACK_1(MenuScene::Play, this));
+	auto menu_item_3 = MenuItemFont::create("Settings", CC_CALLBACK_1(MenuScene::Settings, this));
+	auto menu_item_4 = MenuItemFont::create("Quit", CC_CALLBACK_1(MenuScene::Quit, this));
+
+	
+	menu = Menu::create(menu_item_1, menu_item_3, menu_item_4, NULL);
+	menu->alignItemsVerticallyWithPadding(25.0f);
+	this->addChild(menu);
+
+	auto keyListener = EventListenerKeyboard::create();
+	keyListener->onKeyPressed = CC_CALLBACK_2(MenuScene::onKeyPressed, this);
+	keyListener->onKeyReleased = CC_CALLBACK_2(MenuScene::onKeyReleased, this);
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(keyListener, this);
+
+	return true;
+}
+
+void MenuScene::onKeyPressed(EventKeyboard::KeyCode keyCode, Event *event)
+{
+	log("Key with keycode %d released", keyCode);
+}
+
+void MenuScene::onKeyReleased(EventKeyboard::KeyCode keyCode, Event* event)
+{
+	log("Key with keycode %d released", keyCode);
+}
+
+void MenuScene::Continue(Ref* sender)
+{
+	log("Continue");
+}
+
+void MenuScene::Play(Ref* sender)
+{
+	log("Play");
+}
+
+void MenuScene::Settings(Ref* sender)
+{
+	log("Settings");
+}
+
+void MenuScene::Quit(Ref* sender)
+{
+	exit(0);
+}
+
+/**
+* MenuScene END
+*/
+
+
---
+++ com.DotDatStudios.PoliticallyIncorrect/Classes/Scenes.h
@@ -0,0 +1,53 @@
+#ifndef __PoliticallyIncorrect__Scenes_H__
+#define __PoliticallyIncorrect__Scenes_H__
+
+#include "cocos2d.h"
+#include "Constants.h"
+
+class IScene : public cocos2d::Layer
+{
+public:
+	// a selector callback
+	void menuCloseCallback(cocos2d::Ref* pSender);
+};
+
+class SplashScene : public IScene
+{
+public:
+	// implement the "static create()" method manually
+	CREATE_FUNC(SplashScene);
+
+	INIT_FUNC(SplashScene);
+
+    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+    virtual bool init(); 
+    
+private:
+    void loadMenuScene();
+};
+
+class MenuScene : public IScene
+{
+public:
+	// implement the "static create()" method manually
+	CREATE_FUNC(MenuScene);
+
+	INIT_FUNC(MenuScene);
+
+	// Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+	virtual bool init();
+		
+private:
+	void onKeyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event *event);
+	void onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event *event);
+
+	void Continue(cocos2d::Ref* sender);
+	void Play(cocos2d::Ref* sender);
+	void Settings(cocos2d::Ref* sender);
+	void Quit(cocos2d::Ref* sender);
+
+private:
+	cocos2d::Menu* menu;
+};
+
+#endif // __PoliticallyIncorrect__Scenes_H__
